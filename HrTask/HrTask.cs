97eae8c1769a134036120e555a3dfdc14ee392f7
Tomohito Nakayama <nkym.tmht@gmail.com>
2020-05-10 14:57:08 +0900

    Initial Commit

---
+++ HrTask/HrTask.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace HrTask
+{
+    public class HrTask<H,R> : Task<(H,Task<R>)> 
+    {
+        public HrTask( Func<(H, Task<R>)> func) : base(func) {
+
+        }
+    }
+}
