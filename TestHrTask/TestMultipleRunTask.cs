f8c11f8d1620a973cba7f38e3ae7ebc390deb594
Tomohito Nakayama <nkym.tmht@gmail.com>
2020-05-14 00:22:31 +0900

    Added TestRunAsync in TestMultipleRunTask

--- TestHrTask/TestMultipleRunTask.cs
+++ TestHrTask/TestMultipleRunTask.cs
@@ -29,5 +29,28 @@ namespace TestHrTask
             var remain2Result = remainResult.remainTask.Result;
             Assert.AreEqual(remain2Result, 3);
         }
+
+        [TestMethod]
+        public async Task TestRunAsync()
+        {
+            var multiRunTask = new HrTask<int, HrTask<int, int>>(() =>
+            {
+                return (
+                    1,
+                    () => new HrTask<int, int>(
+                        () => (2,
+                            () => 3)));
+            });
+
+            var result = await multiRunTask;
+            Assert.AreEqual(result.headResult, 1);
+            var remainResultTask = await result.remainTask;
+
+            var remainResult = await remainResultTask;
+            Assert.AreEqual(remainResult.headResult, 2);
+
+            var remain2Result = await remainResult.remainTask;
+            Assert.AreEqual(remain2Result, 3);
+        }
     }
 }
